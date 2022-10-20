8d72b84c6334c2896743ed1ac04adaaee57ba93a
3nsar <84131267+3nsar@users.noreply.github.com>
2022-10-20 21:54:57 +0200

    i bt and added bt searchbar

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import Card from './Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 const url= "https://example-data.draftbit.com/cars?_limit=20"
 
@@ -17,7 +20,17 @@ const Main = () => {
     }, [])
   return (
     <div>
-        <input type="text" onChange={(e) => setSearch(e.target.value)}/>
+        {/*<input type="text" onChange={(e) => setSearch(e.target.value)}/> */}
+        <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <Button variant="outline-secondary" id="button-addon2">
+          Button
+        </Button>
+      </InputGroup>
         {car.filter((item)=>{
             return search.toLowerCase() ==="" ? item : item.make_id.toLowerCase().includes(search)
         }).map((item)=>(
