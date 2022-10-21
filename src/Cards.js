166e1c5a3cb5266b442d2dde3ae52eb0cd542193
3nsar <84131267+3nsar@users.noreply.github.com>
2022-10-21 12:36:31 +0200

    added dynamic cards

---
+++ src/Cards.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const Cards = (props) => {
+  return (
+    /*<div>
+        <h1>{props.maker}</h1>
+        <h1>{props.model}</h1>
+        <h1>{props.year}</h1>
+    </div> */
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={props.image} />
+      <Card.Body>
+        <Card.Title>{props.maker} {props.model}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default Cards
--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import Card from './Card';
+import Cards from './Cards.js';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
@@ -23,7 +23,7 @@ const Main = () => {
   return (
     <div>
         {/*<input type="text" onChange={(e) => setSearch(e.target.value)}/> */}
-        <InputGroup size="lg" className="mb-3" onChange={(e) => setSearch(e.target.value)}>
+        <InputGroup size="lg" className="mb-3 mt-3 w-75"  onChange={(e) => setSearch(e.target.value)}>
         <Form.Control
           placeholder="Recipient's username"
           aria-label="Recipient's username"
@@ -36,10 +36,11 @@ const Main = () => {
         {car.filter((item)=>{
             return search.toLowerCase() ==="" ? item : item.make_id.toLowerCase().includes(search)
         }).map((item)=>(
-            <Card 
+            <Cards 
             maker= {item.make_id}
             model = {item.model}
             year = {item.year}
+            image = {item.image}
             />
         ))}
 
