7ee6228514e3c133a7753dffe0f33aa27a3bc13b
3nsar <84131267+3nsar@users.noreply.github.com>
2022-10-27 15:09:56 +0200

    added pagination

--- src/Cards.js
+++ src/Cards.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 import { Col } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
 
-const Cards = (props) => {
+
+
+const Cards = ({car}) => {
+
   return (
     /*<div>
         <h1>{props.maker}</h1>
@@ -13,20 +17,20 @@ const Cards = (props) => {
     </div> */
 
     <Col >
-    <Card style={{ width: '18rem' }} className="mb-5">
-      <Card.Img variant="top" src={props.image} height="200px" />
+    {car.map((item)=>(
+    <Card  style={{ width: '18rem' }} className="mb-5" key={item.id} >
+      <Card.Img variant="top" src={item.image} height="200px" />
       <Card.Body>
-        <Card.Title>{props.maker} {props.model}</Card.Title>
+        <Card.Title>{item.make_id} {item.model}</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Link to={`/cars/${props.id}`}> <Button>Read more</Button></Link>
+        <Link to={`/cars/${item.id}`}> <Button>Read more</Button></Link>
       </Card.Body>
     </Card>
-
+        ))}
     </Col>
-              
                
   )
 }
--- src/Main.js
+++ src/Main.js
@@ -9,55 +9,17 @@ import Row from  'react-bootstrap/Row'
 import Col from  'react-bootstrap/Col'
 import { Container } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import Pagination from './Pagination.js';
 
 
 
 const url= "https://example-data.draftbit.com/cars?_limit=50"
 
 const Main = () => {
-    function PaginatedItems({ itemsPerPage }) {
-        // We start with an empty list of items.
-        const [currentItems, setCurrentItems] = useState(null);
-        const [pageCount, setPageCount] = useState(0);
-        // Here we use item offsets; we could also use page offsets
-        // following the API or data you're working with.
-        const [itemOffset, setItemOffset] = useState(0);
-      
-        useEffect(() => {
-          // Fetch items from another resources.
-          const endOffset = itemOffset + itemsPerPage;
-          console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-          setCurrentItems(items.slice(itemOffset, endOffset));
-          setPageCount(Math.ceil(items.length / itemsPerPage));
-        }, [itemOffset, itemsPerPage]);
-      
-        // Invoke when user click to request another page.
-        const handlePageClick = (event) => {
-          const newOffset = (event.selected * itemsPerPage) % items.length;
-          console.log(
-            `User requested page number ${event.selected}, which is offset ${newOffset}`
-          );
-          setItemOffset(newOffset);
-        };
-      
-        return (
-          <>
-            <Items currentItems={currentItems} />
-            <ReactPaginate
-              breakLabel="..."
-              nextLabel="next >"
-              onPageChange={handlePageClick}
-              pageRangeDisplayed={5}
-              pageCount={pageCount}
-              previousLabel="< previous"
-              renderOnZeroPageCount={null}
-            />
-          </>
-        );
-      }
-
     const [car, setCar] = useState([])
     const [search, setSearch] = useState("")
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(10);
 
     useEffect(()=>{
         axios.get(url).then((response)=>{
@@ -65,41 +27,24 @@ const Main = () => {
             console.log(response.data)
         })
     }, [])
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = car.slice(indexOfFirstPost, indexOfLastPost);
+  
+    // Change page
+    const paginate = pageNumber => setCurrentPage(pageNumber);
   return (
     <Container>
-        {/*<input type="text" onChange={(e) => setSearch(e.target.value)}/> */}
-        <InputGroup size="lg" className="mb-3 mt-3 w-75"  onChange={(e) => setSearch(e.target.value)}>
-        <Form.Control
-          placeholder="Recipient's username"
-          aria-label="Recipient's username"
-          aria-describedby="basic-addon2"
-        />
-        <Button variant="outline-secondary" id="button-addon2" >
-          <BsSearch />
-        </Button>
-      </InputGroup>
-    
-    
-      <Row >
-     
-        {car.filter((item)=>{
-            return search.toLowerCase() === "" ? item : item.make_id.toLowerCase().includes(search)
-        }).map((item)=>(
-
-            <Cards 
-            maker= {item.make_id}
-            model = {item.model}
-            year = {item.year}
-            image = {item.image}
-            info = {item.description}
-            id = {item.id}
-            />
-
-        ))}
-        
-      
-    </Row> 
-    </Container>
+        <Row>
+            <Cards car={currentPosts}/>
+            <Pagination
+               postsPerPage={postsPerPage}
+               totalPosts={car.length}
+               paginate={paginate}
+      />
+        </Row>
+        </Container>
   )
 }
 
